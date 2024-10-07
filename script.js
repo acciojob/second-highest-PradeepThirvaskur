cb8dbcf74afb5d84b1218e669841f1d48e036724
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-07 14:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
 //your JS code here. If required.
  
-    console.log(secondHighest([5,1,2,3,4]));  
-    console.log(secondHighest([-1,-2,-3,-4,-5]));
-    console.log(secondHighest([1, 1, 1, 1, 1]));
-   console.log(secondHighest([]));
-   console.log(secondHighest([1]));
